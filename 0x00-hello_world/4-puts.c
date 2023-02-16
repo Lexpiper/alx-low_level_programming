43c33b81b3ca9aee311d26f739637e26f3a62938
Ikechukwu Alexander Agwuncha <lexpiperfx@gmail.com>
2023-02-16 12:48:02 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -6,6 +7,7 @@
  */
 int main(void)
 {
-printf("Programming is like building a multilingual puzzle");
+char message[] = "Programming is like building a multilingual puzzle";
+puts(message);
 return (0);
 }
