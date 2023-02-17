edb5609f3dcb56a0b9edf803df8abd6451021f00
Ikechukwu Alexander Agwuncha <lexpiperfx@gmail.com>
2023-02-17 14:14:21 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,19 +12,19 @@ int n, last_digit;
 srand(time(0));
 n = rand();
 /* your code goes there */
-printf("Last digit of %d is %d ", n, last_digit);
 last_digit = abs(n) % 10;
+printf("Last digit of %d is %d ", n, last_digit);
 if (last_digit > 5)
 {
-printf("is greater than 5\n", last_digit);
+printf("is greater than 5\n");
 }
 else if (last_digit == 0)
 {
-printf(" and is 0\n", last_digit);
+printf(" and is 0\n");
 }
 else if (last_digit < 0 && last_digit != 0)
 {
-printf("and is less than 6 and not 0\n", last_digit);
+printf("and is less than 6 and not 0\n");
 }
 return (0);
 }
