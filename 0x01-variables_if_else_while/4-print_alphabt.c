febb9757657f85b4b4b4cdc0f3f38658479e0633
Ikechukwu Alexander Agwuncha <lexpiperfx@gmail.com>
2023-02-17 15:46:19 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints Alphabets in lowercase & uppercase
+ * stored in the variable n
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+putchar('\n');
+return (0);
+}
