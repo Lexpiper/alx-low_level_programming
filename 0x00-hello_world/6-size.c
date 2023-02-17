77ec0d463445faa3b6d807e0faa26fcbe43894ce
Ikechukwu Alexander Agwuncha <lexpiperfx@gmail.com>
2023-02-16 16:06:34 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+*main-A program that prints a line with put function
+*
+*Return:Always 0 (Success)
+*/
 int main(void)
 {
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
