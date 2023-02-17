7014e9ccd2fdf7bf1942474b53bb8a6c58b19354
Ikechukwu Alexander Agwuncha <lexpiperfx@gmail.com>
2023-02-17 09:01:57 -0800

    initial commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ n = rand() - RAND_MAX / 2;
 printf("%d\n", n);
 if (n > 0)
 {
-	printf("is positive\n");
+	printf(" is positive\n");
 }
 else if (n == 0)
 {
-	printf("is zero\n");
+	printf(" is zero\n");
 }
 else if (n < 0)
 {
-	printf("is negative\n");
+	printf(" is negative\n");
 }
 return (0);
 }
