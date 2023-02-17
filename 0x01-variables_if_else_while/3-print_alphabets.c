a3b0a61449bc9170a2ecc1c78c07e97b553babee
Ikechukwu Alexander Agwuncha <lexpiperfx@gmail.com>
2023-02-17 15:13:43 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints Alphabets in lowercase & uppercase
+ * stored in the variable n
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char c;
+
+for (c = a; c <= z; c++)
+	putchar(c)
+for (c = A; c <= z; c++)
+	putchar(c)
+putchar('\n')
+return (0)
+}
