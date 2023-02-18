2da3dd3878d515a02ab95008d5747676c1693ab9
Ikechukwu Alexander Agwuncha <lexpiperfx@gmail.com>
2023-02-18 02:50:04 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,7 +8,7 @@ int main(void)
 {
 int n1;
 int n2;
-for (n1 = 0; n1 < 10; n1++)
+for (n1 = 0; n1 < 9; n1++)
 {
 for (n2 = 1; n2 < 10; n2++)
 {
@@ -19,10 +19,10 @@ putchar(n2 + '0');
 if (n1 != 8 || n2 != 9)
 {
 putchar(',');
+putchar(' ');
 }
 }
 }
-putchar('\n');
 }
 return (0);
 }
