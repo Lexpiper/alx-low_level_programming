1b1f0995aabb8b4e26c5075e1eea31acdc798b4f
Ikechukwu Alexander Agwuncha <lexpiperfx@gmail.com>
2023-02-17 17:14:22 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,11 +6,13 @@
  * Return: Always 0 (Success)
  */
 
-int main(void) {
+int main(void)
+{
 int i;
-for (i = 0; i < 10; i++) {
-putchar(i + '0');
+for (i = 0; i < 10; i++)
+{
+	putchar(i + '0');
 }
 putchar('\n');
-return 0;
+return (0);
 }
