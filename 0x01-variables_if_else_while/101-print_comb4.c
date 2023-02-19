bb006129e5d50fe66afa61f4eddce80b66744205
Ikechukwu Alexander Agwuncha <lexpiperfx@gmail.com>
2023-02-19 09:01:38 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,18 +14,19 @@ for (n1 = 1; n1 < 10; n1++)
 	{
 		for (n3 = n2 + 1; n3 < 10; n3++)
 		{
-			if (n1 != n2 && n2 != n3 && n1 != n3)
+			if (n1 != n2 && n1 != n3 && n2 != n3)
 			{
-				putchar(n1 + '0');
-				putchar(n2 + '0');
-				putchar(n3 + '0');
-				if (n1 != 7 && n2 != 8 && n3 != 9)
+				if (n1 < n2 && n2 < n3)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(n1 + '0');
+					putchar(n2 + '0');
+					putchar(n3 + '0');
+					if (n1 != 7 || n2 != 8 || n3 != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
-
-
 			}
 		}
 	}
