08eb4b802508d67a8fcbdf27a033d1ad3191b856
Ikechukwu Alexander Agwuncha <lexpiperfx@gmail.com>
2023-02-21 15:51:01 -0800

    8-24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,32 +1,22 @@
 #include "main.h"
-
 /**
 *jack_bauer - prints every minute of the day of jack bauer
 *Return: ...
 **/
-
 void jack_bauer(void)
 {
-	int a, b, c, d;
-	for (a = 0; a <= 2; a++)
+int hours, minutes;
+
+	for (hours = 0; hours < 24; hours++)
 	{
-	for (b = 0; b <= 9; b++)
+        for (minutes = 0; minutes < 60; minutes++)
 	{
-	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
-	{
-	for (c = 0; c <= 5; c++)
-	{
-	for (d = 0; d <= 9; d++)
-	{
-	_putchar(a + '0');
-	_putchar(b + '0');
-	_putchar(50);
-	_putchar(c + '0');
-	_putchar(d + '0');
-	_putchar('\n');
-	}
-	}
-	}
-	}
+        _putchar('0' + (hours / 10));
+        _putchar('0' + (hours % 10));
+        _putchar(':');
+        _putchar('0' + (minutes / 10));
+        _putchar('0' + (minutes % 10));
+        _putchar('\n');
+        }
 	}
 }
