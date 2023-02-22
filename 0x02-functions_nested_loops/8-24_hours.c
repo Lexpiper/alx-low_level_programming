64c9b02d71754e83326f8757072e27e75f788d1f
Ikechukwu Alexander Agwuncha <lexpiperfx@gmail.com>
2023-02-21 16:00:01 -0800

    8-24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,16 +7,16 @@ void jack_bauer(void)
 {
 int hours, minutes;
 
-	for (hours = 0; hours < 24; hours++)
-	{
-        for (minutes = 0; minutes < 60; minutes++)
-	{
-        _putchar('0' + (hours / 10));
-        _putchar('0' + (hours % 10));
-        _putchar(':');
-        _putchar('0' + (minutes / 10));
-        _putchar('0' + (minutes % 10));
-        _putchar('\n');
-        }
-	}
+for (hours = 0; hours < 24; hours++)
+{
+for (minutes = 0; minutes < 60; minutes++)
+{
+_putchar('0' + (hours / 10));
+_putchar('0' + (hours % 10));
+_putchar(':');
+_putchar('0' + (minutes / 10));
+_putchar('0' + (minutes % 10));
+_putchar('\n');
+}
+}
 }
