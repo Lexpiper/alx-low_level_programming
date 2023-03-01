8d3132a30368cee1a5097f0784836d4c977fc79f
Ikechukwu Alexander Agwuncha <lexpiperfx@gmail.com>
2023-02-28 20:27:05 -0800

    100-main.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include"main.h"
+
+/**
+ *_atoi - converts string to integer
+ *@s: string to be converted
+ *Return: 0 on success
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int digit;
+
+	while (*s)
+	{
+	if (*s == '-')
+	{
+	sign = -1;
+	}
+	else if (*s == '+')
+	{
+	sign = 1;
+	}
+	else if (*s >= '0' && *s <= '9')
+	{
+	digit = *s - '0';
+	result = result * 10 + digit;
+	}
+	else if (result > 0)
+	{
+	break;
+	}
+	s++;
+	}
+	return (result * sign);
+}
