4f393ec33943b1c408f635a1c15d93e45a911ca7
Lexpiper <lexpiperfx@gmail.com>
2023-03-22 21:44:21 +0100

    prints the individual number in

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
+/**
+ * array_iterator - prints individua elem in an array
+ * @array: array
+ * @size: how many elem to print
+ * @action: pointer to print in regular or hex
+ * Return: void
+*/
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
