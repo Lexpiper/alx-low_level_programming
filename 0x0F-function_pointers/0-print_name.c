48d59097c4ecb555b63b68921d6b98ec9e1161d6
Lexpiper <lexpiperfx@gmail.com>
2023-03-22 20:16:54 +0100

    prints name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 /**
- * 
- * 
- * 
+ * print_name - pointer function that calls another func
+ * @name: charter to print
+ * @f: funtion pointer
 */
 
 void print_name(char *name, void (*f)(char *))
