10367ac23e85d4dd1750ab24b7def0a4c97a0a28
Lexpiper <lexpiperfx@gmail.com>
2023-03-23 22:48:07 +0100

    sum all numbers

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,18 +4,19 @@
 /**
  * sum_them_all - sums
  * @n: count of arguments
- * ... :  A variable number of parameters to calulate the sum of
+ * 
  * Return: return 0 if n == o
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list sum;
+
+	unsigned int i;
+	int result = 0;
+
 	if (n == 0)
 		return 0;
-
-	va_list sum;
-	unsigned int i, result = 0;
-
 	va_start(sum, n);
 
 	for (i = 0; i < n; i++)
