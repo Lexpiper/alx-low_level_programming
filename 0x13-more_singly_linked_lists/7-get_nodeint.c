30507fb7f6535ed671f83ddc84769a1741a46f25
Ikechukwu Alexander Agwuncha <lexpiperfx@gmail.com>
2023-04-04 21:44:05 -0700

    gets the nth node in a linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -6,7 +6,7 @@
  *@index: index of the node starting at zero
  *
  *Return: Nth node o the list
- 	  or NULL if the  node doesn't exist
+ *	or NULL if the  node doesn't exist
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
