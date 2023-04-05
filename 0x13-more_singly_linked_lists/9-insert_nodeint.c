9d730b248ab695ca54ca32b2a331be7dfb6608a5
Ikechukwu Alexander Agwuncha <lexpiperfx@gmail.com>
2023-04-04 22:30:40 -0700

    inserts nodde at nth position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- *insert_nodeint_nodeint_at_index -  inserts a new node at a given position
+ *insert_nodeint_at_index -  inserts a new node at a given position
  *@head: pointer to the linked list
  *@idx: index of the list where the new node should be added
  *@n: the value of the node
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp, *newnode;
 
 	unsigned int count = 1;
-	
+
 	temp = *head;
 
 	if (head == NULL)
