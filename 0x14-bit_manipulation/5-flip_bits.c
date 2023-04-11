d9de8e453662366eb8f538e3b1d0e9fe03b54907
Ikechukwu Alexander Agwuncha <lexpiperfx@gmail.com>
2023-04-11 07:23:28 -0700

    flips bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,10 +12,11 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int bits;
 
-	for (bit = 0; n || m; n >> 1, m >>= 1)
+	for (bits = 0; n || m; n >>= 1, m >>= 1)
+	
 	{
 		if ((n & 1) != (m & 1))
-			bit++
+			bits++;
 	}
 
 	return (bits);
