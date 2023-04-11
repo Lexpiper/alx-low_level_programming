aaa413b1ff29ac7ba209b3a48efbbd7b6ed85d1f
Ikechukwu Alexander Agwuncha <lexpiperfx@gmail.com>
2023-04-11 02:06:38 -0700

    reads and prints to stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,7 +2,7 @@
 
 /**
  *read_textfile - reads a text file and prints it to the POSIX standard output.
- *@filename: pointer to file 
+ *@filename: pointer to file
  *@letters: number of letters it should read and print
  *
  * Return: actual numbers of letters that could be read and print
@@ -16,42 +16,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	/*open file*/
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
-		free(buffer);
 		return (0);
-	}
-
-	/*allocate memory for buffer*/
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
-	/*read the content of the file*/
 	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
-	{
-		free(buffer);
 		return (0);
-	}
 
-	/*write the content of the file to the standard output*/
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written == -1)
-	{
-		free(buffer);
 		return (0);
-	}
 
-	/*close file and free buffer*/
 	close(fd);
 	free(buffer);
 
 	return (bytes_written);
-
-
 }
 
