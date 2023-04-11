f3ade8d39e8e24c4a4b5542f0fb939a0c057b5ed
Ikechukwu Alexander Agwuncha <lexpiperfx@gmail.com>
2023-04-11 07:18:48 -0700

    flips bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *flip_bits - returns the number of bit needed to be flipped
+ *@n: first Number
+ *@m: Second number
+ *
+ * Return: the Number of bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int bits;
+
+	for (bit = 0; n || m; n >> 1, m >>= 1)
+	{
+		if ((n & 1) != (m & 1))
+			bit++
+	}
+
+	return (bits);
+
+}
+
