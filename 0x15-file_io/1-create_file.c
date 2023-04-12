1e3251d8d9fa36e66efd113c3b12638a3afe20dd
Ikechukwu Alexander Agwuncha <lexpiperfx@gmail.com>
2023-04-12 07:14:08 -0700

    creates file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,15 +22,15 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		while(text_content[i])
+		while (text_content[i])
 			i++;
-	
+
 		bytes_written = write(fd, text_content, i);
 
 		if (bytes_written == -1)
 		{
 			close(fd);
-			return(-1);
+			return (-1);
 		}
 	}
 	close(fd);
