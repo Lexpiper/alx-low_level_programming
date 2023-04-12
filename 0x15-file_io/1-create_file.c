9cbd0d2034ca722d2c42a50adab19f3e40b15e32
Ikechukwu Alexander Agwuncha <lexpiperfx@gmail.com>
2023-04-12 07:06:36 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,34 +4,37 @@
 /**
  *create_file - creates a file
  *@filename: name of file
- *@text_content:string to write to file
+ *@text_content: string to write to file
  *
  *Return: 1 if file exists and -1 if file doesnt exist
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, bytes_written = 0;
-	mode_t mode = S_IRUSR | S_IWUSR;
-	
-	if (filename == NULL)
-		return (0);
+	int fd, bytes_written, i = 0;
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
-	if (fd == 0)
+	if (!filename)
 		return (-1);
 
-	if (text_content == NULL)
-		bytes_written = write(fd, text_content, strlen(text_content));
-	else
-		bytes_written = 0;
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
-	if (bytes_written == -1)
+	if (text_content)
 	{
-		close(fd);
-		return (-1);
+		while(text_content[i])
+			i++;
+	
+		bytes_written = write(fd, text_content, i);
+
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return(-1);
+		}
 	}
 	close(fd);
+
 	return (1);
 }
 
