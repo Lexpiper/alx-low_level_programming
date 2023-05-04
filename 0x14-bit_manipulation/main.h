05c955a96b4ad0b823683fed825a5b00f301b3dd
Lexpiper <lexpiperfx@gmail.com>
2023-05-04 13:47:32 +0100

    adds header file

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,15 @@
+#ifndef BIT_H
+#define BIT_H
+
+/*THE LIBRARIES*/
+#include <stdio.h>
+#include <string.h>
+
+/*THE PROTOTYPES*/
+int _putchar(char);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+
+
+#endif
+
